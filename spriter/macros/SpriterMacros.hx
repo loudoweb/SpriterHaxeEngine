0fa46050cfdd308c2122d1f2cd3b8ad46aa38d17
loudoweb <loudoweb@gmail.com>
2014-04-29 16:34:55 +0200

    Added macro to create a timeline with image ordering for spriter b7. Not very useful since the import frames feature of Spriter b8.

--- spriter/macros/SpriterMacros.hx
+++ spriter/macros/SpriterMacros.hx
@@ -17,20 +17,20 @@ class SpriterMacros
 	 */
 	macro public static function texturePackerChecker(pathToXml:Expr) : Expr 
 	{
-		var xml_loc:String = '';
+		var xml_path:String = '';
 		switch(pathToXml.expr){
 			case EConst(c): switch(c){
-				case CString(s): xml_loc = s; 
+				case CString(s): xml_path = s; 
 				default:{}
 			}
 			default:{}
 		}
-		trace('checking texture packer file : '+xml_loc);
-		var xml_s = sys.io.File.getContent(xml_loc);
+		trace('checking texture packer file : '+xml_path);
+		var xml_s = sys.io.File.getContent(xml_path);
 		var xml = Xml.parse(xml_s);
 		var save:Bool = parseTexturePacker(xml.firstElement());
 		if(save)
-			sys.io.File.saveContent(xml_loc, xml.toString());
+			sys.io.File.saveContent(xml_path, xml.toString());
 		
 		return pathToXml;
 	}
@@ -57,5 +57,157 @@ class SpriterMacros
 		if (corrected)
 			trace('texturePacker XML is corrected.');
 		return corrected;
+	}
+	/**
+	 * Macro to create an animation with image ordering starting with offset number. Not very useful since Spriter b8.
+	 * @example SpriterMacros.createSpriterTimeline('assets/test.scml', 35, 0, 'folder/subfolder', 'myImage_', 200, 100, 24);
+	 * will create an scml with 35 images at 24 fps like these data :
+	 * <file id="0" name="folder/subfolder/myImage_0.png" width="200" height="100" pivot_x="0" pivot_y="1"/>
+	 * <key id="0" time="0">
+     *      <object_ref id="0" timeline="0" key="0" z_index="0"/>
+     * </key>
+	 * etc.
+	 * 
+	 * @param	pathToXml where the .scml file is
+	 * @param	frames number of total key frames
+	 * @param	offset if your image name doesn't start with 0
+	 * @param	folder the folder name
+	 * @param	filePrefix the file prefix (before the number)
+	 * @param	width of image
+	 * @param	height of image
+	 * @param	framerate of the animation
+	 * @return  pathToXml
+	 */
+	macro public static function createSpriterTimeline(pathToXml:Expr, frames:Expr, offset:Expr, folder:Expr, filePrefix:Expr, width:Expr, height:Expr, framerate:Expr):Expr
+	{
+		var xml_path:String = '';
+		var frames_num:Int = 0;
+		var offset_num:Int = 0;
+		var file_prefix:String = '';
+		var _folder:String = '';
+		var _width:Int = 0;
+		var _height:Int = 0;
+		var _framerate:Int = 24;
+		switch(pathToXml.expr){
+			case EConst(c): switch(c){
+				case CString(s): xml_path = s; 
+				default:{}
+			}
+			default:{}
+		}
+		switch(folder.expr){
+			case EConst(c): switch(c){
+				case CString(s): _folder = s; 
+				default:{}
+			}
+			default:{}
+		}
+		switch(filePrefix.expr){
+			case EConst(c): switch(c){
+				case CString(s): file_prefix = s; 
+				default:{}
+			}
+			default:{}
+		}
+		switch(frames.expr){
+			case EConst(c): switch(c){
+				case CInt(s): 
+					frames_num = Std.parseInt(s); 
+				default:{}
+			}
+			default:{}
+		}
+		switch(framerate.expr){
+			case EConst(c): switch(c){
+				case CInt(s): _framerate = Std.parseInt(s); 
+				default:{}
+			}
+			default:{}
+		}
+		switch(offset.expr){
+			case EConst(c): switch(c){
+				case CInt(s): offset_num = Std.parseInt(s); 
+				default:{}
+			}
+			default:{}
+		}
+		switch(width.expr){
+			case EConst(c): switch(c){
+				case CInt(s): _width = Std.parseInt(s); 
+				default:{}
+			}
+			default:{}
+		}
+		switch(height.expr){
+			case EConst(c): switch(c){
+				case CInt(s): _height = Std.parseInt(s); 
+				default:{}
+			}
+			default:{}
+		}
+		trace('creating timeline file : '+xml_path);
+		var xml:Xml = createSCML(_folder, frames_num, offset_num, _framerate, file_prefix, _width, _height);
+		sys.io.File.saveContent(xml_path, xml.toString());
+		
+		return pathToXml;
+	}
+	static function createSCML(folder:String, frames:Int, offset:Int, framerate:Int, filePrefix:String, _width:Int, _height:Int ):Xml
+	{
+		var tag:String;
+		var done:Bool = false;
+		var totalTime:Int = Std.int((frames / framerate) * 1000);
+		var stepTime:Int =  Std.int(1000 / framerate);
+		var xml_s:String = '<?xml version="1.0" encoding="UTF-8"?>\n'+
+		'<spriter_data scml_version="1.0" generator="SpriterHaxeEngine" generator_version="b7">\n'+
+		'	<folder id="0" name="'+folder+'">\n'+
+		'	</folder>\n'+
+		'	<entity id="0" name="entity_000">\n'+
+		'		<animation id="0" name="NewAnimation" length="'+totalTime+'">\n'+
+		'			<mainline>\n'+
+		'			</mainline>\n'+
+		'			<timeline id="0" name="'+filePrefix+'">\n'+
+		'			</timeline>\n'+
+		'		</animation>\n'+
+		'	</entity>\n'+
+		'</spriter_data>\n';
+		
+		var xml:Xml = Xml.parse(xml_s).firstElement();
+		for (el in xml.elements())
+		{
+			if (el.nodeName == 'folder') {
+				for (i in 0...(frames+1))
+				{
+					tag = '		<file id="' + i + '" name="' + folder + '/' + filePrefix + (i+offset) + '.png" width="' + _width + '" height="' + _height + '" pivot_x="0" pivot_y="1"/>\n';
+					el.addChild(Xml.parse(tag));
+				}
+			}else if (el.nodeName == 'entity') {
+				var ent:Xml = el.firstElement();
+				if (ent.nodeName == 'animation') {
+					for (anim in ent.elements())
+					{
+						if (anim.nodeName == 'mainline') {
+							for (i in 0...(frames+1))
+							{	
+								tag = 
+								'	<key id="'+i+'" time="'+i*stepTime+'">\n'+
+								'		<object_ref id="'+i+'" timeline="0" key="'+i+'" z_index="0"/>\n'+
+								'	</key>\n';
+								anim.addChild(Xml.parse(tag));
+							}
+						}else if (anim.nodeName == 'timeline') {
+							for (i in 0...(frames+1))
+							{	
+								tag = 
+								'	<key id="'+i+'" time="'+i*stepTime+'" spin="0" >\n'+
+								'		<object folder="0" file="'+i+'"/>\n'+
+								'	</key>\n';
+								anim.addChild(Xml.parse(tag));
+							}
+						}
+					}
+				}
+			}
+		}
+		return xml;
 	}
 }
