718c1d62571602d036147ed17515d6f82744026d
loudoweb <loudoweb@gmail.com>
2017-08-08 15:55:01 +0200

    fix #37 for haxe >3.3

--- spriter/definitions/Metaline.hx
+++ spriter/definitions/Metaline.hx
@@ -5,19 +5,26 @@ import haxe.xml.Fast;
  * ...
  * @author Loudo
  */
+
+//Internal helpers
+#if (haxe_ver < 3.3)
 typedef ConstructibleKey = {
   public function new(fast:Fast):Void;
 }
+#end
  
 @:generic
-class Metaline<T:ConstructibleKey>
-{
+#if (haxe_ver >= 3.3)
+class Metaline<T:haxe.Constraints.Constructible<Fast->Void>> {
+#else
+class Metaline<T:ConstructibleKey> {
+#end
 
 	public var id:Int;
 	public var name:String;
 	public var keys:Array<T>;
 	
-	public function new(fast:Fast = null) 
+	public function new(fast:Fast) 
 	{
 		if(fast != null){
 			id = fast.has.def ? Std.parseInt(fast.att.def) : fast.has.id ? Std.parseInt(fast.att.id) : -1;
